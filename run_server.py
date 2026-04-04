aea154b0dd9ceeb923a9d2a1f7ace69938fbcd09
baptiste-an <96290313+baptiste-an@users.noreply.github.com>
2026-04-04 06:57:15 +0100

    updated data

--- run_server.py
+++ run_server.py
@@ -1,11 +1,10 @@
-from waitress import serve
-from index import (
-    app,
-)  # Make sure 'index' is your main file and 'app' is the Dash instance
 import os
 
+from waitress import serve
+
+from index import server
+
+
 if __name__ == "__main__":
-    port = int(
-        os.environ.get("PORT", 8050)
-    )  # Utilise le port fourni par Render ou 8050 par défaut
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8050))
+    serve(server, host="0.0.0.0", port=port, threads=8)
